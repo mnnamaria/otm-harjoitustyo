5ab4f3a5bc4fb78962db58a249dcbecd3dd8cae8
Minna Hirvonen <minna.m.hirvonen@helsinki.fi>
2018-04-05 14:20:19 +0300

    uusi peli

--- TicTacToe/src/main/java/tictactoe/Main.java
+++ TicTacToe/src/main/java/tictactoe/Main.java
@@ -15,8 +15,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Board lauta = new Board();
-        lauta.setToBoard("X",1);
+        Game lauta = new Game();
+        lauta.playGame();
     }
     
 }
